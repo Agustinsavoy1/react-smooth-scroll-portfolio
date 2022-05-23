048b0d3e2bf5bda7c62841e34e63ba3e5bbe8e5d
Agustin <agustinsavoy1@gmail.com>
2022-05-23 16:27:51 +0200

    v1.0

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,149 @@
+import React, { useState } from 'react';
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaCamera,
+  FaYoutube,
+  FaInstagram,
+} from 'react-icons/fa';
+// import Logo from '../assets/logo.png';
+import { Link } from 'react-scroll';
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        {/*<img src={Logo} alt='Logo' style={{ width: '50px' }} />*/}
+      </div>
+
+      {/* menu */}
+      <ul className='hidden md:flex'>
+        <li>
+          <Link to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* Social icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-black-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://github.com/Agustinsavoy1'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-orange-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://agustinsavoy-portfolio.herokuapp.com/'
+            >
+              Audiovisual <FaCamera size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-red-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://www.youtube.com/channel/UC86dkdMChJP8VX1tcu6eiCA'
+            >
+              Youtube <FaYoutube size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-rose-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://www.instagram.com/agustinsavoy.json/'
+            >
+              Instagram <FaInstagram size={30} />
+            </a>
+          </li>
+          
+          
+          
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Work.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import WorkImg from "../assets/workImg.jpeg";
+
+const Work = () => {
+  const [demo, setDemo] = useState([]);
+  const [repoList, setRepoList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://api.github.com/users/agustinsavoy1")
+      .then((response) => response.json())
+      .then(
+        (data) => {
+          console.log(data);
+          setDemo(data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+
+      //Aca va el otro fetch Enviar CV a hello@selection-it.com para la posicion!
+  }, []);
+
+  useEffect(() => {
+    fetch("http://api.github.com/users/agustinsavoy1/repos")
+      .then((response) => response.json())
+      .then(
+        (data) => {
+          console.log(data);
+          
+          setRepoList(data)
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
+  return (
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">// Check out some of my recent work</p>
+        </div>
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Item */}
+              {repoList.slice(0,6).map((repo) => {
+                return (
+                <div key={repo.id}
+                style={{ backgroundImage: `url(${WorkImg})` }}
+                className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+              >
+                {/* Hover Effects */}
+                <div className="opacity-0 group-hover:opacity-100">
+                  <span className="text-2xl font-bold text-white tracking-wider text-justify">
+                    {repo.name}
+                  </span>
+                  <div className="pt-8 text-center">
+                    <a href={demo.blog}>
+                      <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                        Demo
+                      </button>
+                    </a>
+                    <a target='_blank' rel="noreferrer" href={repo.html_url}>
+                      <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                        Code
+                      </button>
+                    </a>
+                  </div>
+                </div>
+              </div>)})}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
